ac2409c1d4b7745fd1add625d69edb018f8f129a
Alberto Miranda <codealchemist@gmail.com>
2019-02-05 23:15:45 -0300

    Forcing snapshots to be updated locally on test script; updated snapshots; mocked dates as strings to avoid timezone errors.

--- src/components/Message/Message.test.js
+++ src/components/Message/Message.test.js
@@ -5,7 +5,7 @@ import Message from './Message'
 describe('Message', () => {
   const user = { id: 42, name: 'Steve Vai', avatar: '7-string-guitar' }
   const text = 'For the love of God'
-  const date = new Date('2019-02-04T23:55:17.782Z')
+  const date = '2019-02-04T23:55:17.782Z'
 
   it('should render', () => {
     const wrapper = mount(<Message user={user} text={text} date={date} />)
--- src/components/Notification/Notification.test.js
+++ src/components/Notification/Notification.test.js
@@ -3,11 +3,14 @@ import { mount } from 'enzyme'
 import Notification from './Notification'
 
 const _Date = Date
-const date = new Date('2019-02-04T23:55:17.782Z')
+const date = '2019-02-04T23:55:17.782Z'
 global.Date = jest.fn(function () {
   return date
 })
 global.Date.now = () => 1549382820579
+jest.mock('moment', () => () => ({
+  format: () => 'February 4th 2019, 11:55:17 pm'
+}))
 
 describe('Notification', () => {
   it('should render', () => {
